a8bb08c9169d61901af5b84b57d426051dfb24b9
Emerick <mricszonyi@gmail.com>
2022-10-11 09:19:23 +0200

    Completing the assignment

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/html/index.html');
 })
 
+app.get('/api/task', (req, res) => {
+    console.log(req.query)
+    if (req.query.value== 'Kitchen'){
+        res.send([{Nom:"Washing the dishes"},{Nom:"Cleaning the ground"},{Nom:"Buying Food" }])
+    }
+    if (req.query.value== 'Bedroom'){
+        res.send([{Nom:"Cleaning the bedroom"},{Nom:"Cleaning the ground"},{Nom:"clean up one's stuff" }])
+    }
+    if (req.query.value== 'Outside'){
+        res.send([{Nom:"Taking out the trash"},{Nom:"To mow the lawn"},{Nom:"Water the plants" }])
+    }
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
